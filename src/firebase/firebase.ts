df59bccac200afda131af7f7867f15dc0288208a
domkab <dominykas.bobinas@gmail.com>
2025-07-01 17:32:04 +0300

    firebase integration + env vars

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -6,11 +6,12 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "next-blog-acbbc.firebaseapp.com",
-  projectId: "next-blog-acbbc",
-  storageBucket: "next-blog-acbbc.appspot.com",
-  messagingSenderId: "747614347281",
-  appId: "1:747614347281:web:bf168be2a99bc06aee51b0",
+  authDomain: "gargofx-cdd6b.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: "57764844648",
+  appId: "1:57764844648:web:f3d056984926757a41d7dd",
+  measurementId: "G-BB9FG5S3D7"
 };
 
 // Initialize Firebase

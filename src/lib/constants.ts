3c04edcf34b471959e95526b0614ee1aa8a8327b
domkab <dominykas.bobinas@gmail.com>
2025-07-02 15:50:33 +0300

    new metadata injection method

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Site Title Example';
-export const SITE_NAME = 'example';
+export const SITE_TITLE = 'GargoFX';
+export const SITE_NAME = 'gargofx'; // lowercase for URLs
 export const DOMAIN_NAME = 'example.com';
 export const CHARACTER_NAME = 'Pixel';

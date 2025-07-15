6436fdde4ba9d48336d39bb73d1b579ce52f7bc4
domkab <dominykas.bobinas@gmail.com>
2025-07-15 19:53:41 +0300

    remove commented code

--- src/types/featuredLayout.ts
+++ src/types/featuredLayout.ts
@@ -1,24 +1,3 @@
-// export type LayoutSize = '1/3' | '1/2' | '2/3' | 'full';
-
-// export interface FeaturedBlock {
-//   id: string;
-//   postId: string;
-//   layout: LayoutSize;
-//   image?: {
-//     desktop: { url: string; alt?: string };
-//     mobile?: { url: string; alt?: string };
-//   };
-// }
-
-// export interface FeaturedLayoutRowBase {
-//   order: number;
-//   blocks: FeaturedBlock[];
-// }
-
-// export interface FeaturedLayoutRow extends FeaturedLayoutRowBase {
-//   _id?: string;
-// }
-
 export type LayoutSize = '1/3' | '1/2' | '2/3' | 'full';
 
 export interface FeaturedBlock {

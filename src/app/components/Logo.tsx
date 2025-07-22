fff01df4cb3c409598c646527756ff86e1d2c1bb
domkab <dominykas.bobinas@gmail.com>
2025-07-22 13:03:15 +0300

    header with burger basics and underline styles

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import clsx from 'clsx';
 
-export default function Logo() {
+type LogoProps = {
+  className?: string;
+};
+
+export default function Logo({ className }: LogoProps) {
   return (
-    <Link
-      href="/"
-      className="self-center"
-    >
+    <Link href="/" className={clsx('self-center', className)}>
       <Image
         src="/icons/Logo.svg"
         alt="GARGOFX Logo"

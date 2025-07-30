f891fbcbf3bd6b09640b8c537569ee2643ed58f3
domkab <dominykas.bobinas@gmail.com>
2025-07-30 15:46:49 +0300

    about articles mobile version

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,21 +28,95 @@ export default async function AboutPage() {
           <h1
             className={clsx(
               styles['about__text'],
-              'font-semibold'
+              'mb-1'
             )}
           >
             About us
           </h1>
 
-          <h2
+          <p
             className={clsx(
               styles['about__text'],
               styles['about__text--secondary'],
             )}
           >
             Who we are and what we do
-          </h2>
+          </p>
         </div>
+      </section>
+
+      <section className={clsx(styles['about__content'])}>
+        <h2 className={clsx(styles['about__content-title'])}>
+          We’re a creative team specializing in 3D design, animation, and visual effects. From stylized content to photorealistic renders, we help brands and studios bring ideas to life.
+        </h2>
+
+        <article className={clsx(styles['about__article'])}>
+          <h3 className={clsx(styles['about__subheading'])}>
+            3D Modeling
+          </h3>
+          <p className={clsx(styles['about__paragraph'])}>
+            Custom-built characters, environments, props, and product models for animation,
+            games, and visualizations.
+          </p>
+        </article>
+
+        <article className={clsx(styles['about__article'])}>
+          <h3 className={clsx(styles['about__subheading'])}>
+            3D animation
+          </h3>
+          <p className={clsx(styles['about__paragraph'])}>
+            Full-service animation including character animation, creature animation, motion graphics, and explainer animations. Ideal for film, games, and branded content.
+          </p>
+        </article>
+
+        <article className={clsx(styles['about__article'])}>
+          <h3 className={clsx(styles['about__subheading'])}>
+            Game trailers & Cinematics
+          </h3>
+          <p className={clsx(styles['about__paragraph'])}>
+            High-impact trailers, teaser videos, and in-game cinematics that bring your game’s story and style to life. Perfect for launches, Steam pages, and social media campaigns.
+          </p>
+        </article>
+
+        <article className={clsx(styles['about__article'])}>
+          <h3 className={clsx(styles['about__subheading'])}>
+            Product visualization
+          </h3>
+          <p className={clsx(styles['about__paragraph'])}>
+            Photorealistic or stylized 3D renders of products for ads, websites, and e-commerce platforms.
+          </p>
+        </article>
+
+        <article className={clsx(styles['about__article'])}>
+          <h3 className={clsx(styles['about__subheading'])}>
+            Commercial & Advertising Animation
+          </h3>
+          <p className={clsx(styles['about__paragraph'])}>
+            3D commercials tailored for online, TV, or social platforms. Includes stylized ads, animated promos, and branded storytelling.
+          </p>
+        </article>
+
+        <article className={clsx(styles['about__article'])}>
+          <h3 className={clsx(styles['about__subheading'])}>
+            Visual Effects (VFX)
+          </h3>
+          <p className={clsx(styles['about__paragraph'])}>
+            Smoke, fire, fluid simulations, destruction, particle effects, and compositing for dynamic video projects.
+          </p>
+        </article>
+
+        <article className={clsx(styles['about__article'])}>
+          <h3 className={clsx(styles['about__subheading'])}>
+            3D for AR/VR
+          </h3>
+          <p className={clsx(styles['about__paragraph'])}>
+            Optimized 3D assets and immersive environments for augmented and virtual reality platforms.
+          </p>
+        </article>
+      </section>
+
+      <section>
+        <div className="">suktukas</div>
       </section>
     </main>
   );

73aec1fceeb99c5c6a8087217adf040deb19ead8
domkab <dominykas.bobinas@gmail.com>
2025-07-31 19:42:12 +0300

    contact page hero + rename styling

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,23 +1,65 @@
 import clsx from 'clsx';
-import styles from '@/styles/components/contact.module.scss';
 import ContactForm from '../components/Contact/ContactForm';
+import HomeImageCarousel from '../components/HomeImageCarousel';
+import { getCarouselImages } from '@/lib/services/imageService';
+import contactForm from '@/styles/components/contact.module.scss';
+import contactPage from '@/styles/components/contactPage.module.scss';
 
-export default function ContactPage() {
+export default async function ContactPage() {
+  const images = await getCarouselImages();
+
   return (
-    <main className={clsx(styles.contact, 'px-5 py-5')}>
-      <h1 className={clsx(styles.title, 'text-white text-3xl mb-8')}>contact</h1>
+    <main
+      className={clsx(
+        contactPage['contact'], 'contact'
+      )}>
 
-      <div className={clsx(styles.socials, 'flex gap-8 text-secondary mb-10')}>
-        <a href="https://facebook.com" target="_blank" className={styles.link}>
-          facebook
-        </a>
-        <a href="https://instagram.com" target="_blank" className={styles.link}>
-          instagram
-        </a>
-        <button className={styles.close}>x</button>
-      </div>
+      <section className={clsx('contact__hero', 'relative', 'w-full')}>
+        <HomeImageCarousel images={images} />
 
-      <ContactForm />
+        <div
+          className={clsx(
+            contactPage['contact__hero-text-container'],
+            'text-start',
+            'text-white',
+            'z-10',
+          )}
+        >
+          <h1
+            className={clsx(
+              contactPage['contact__hero-text'],
+              'mb-1'
+            )}
+          >
+            About us
+          </h1>
+
+          <p
+            className={clsx(
+              contactPage['contact__hero-text'],
+              contactPage['contact__hero-text--secondary'],
+            )}
+          >
+            Who we are and what we do
+          </p>
+        </div>
+      </section>
+
+      <section className={clsx(contactPage['contact__content'])}>
+        <h1 className={clsx(contactForm.title, 'text-white text-3xl mb-8')}>contact</h1>
+
+        <div className={clsx(contactForm.socials, 'flex gap-8 text-secondary mb-10')}>
+          <a href="https://facebook.com" target="_blank" className={contactForm.link}>
+            facebook
+          </a>
+          <a href="https://instagram.com" target="_blank" className={contactForm.link}>
+            instagram
+          </a>
+          <button className={contactForm.close}>x</button>
+        </div>
+
+        <ContactForm />
+      </section>
     </main>
   );
 }

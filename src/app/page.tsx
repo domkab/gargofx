fd03a3dfb9bf3d2741f3b1163eb799d6dfc1bd86
domkab <dominykas.bobinas@gmail.com>
2025-08-06 19:05:19 +0300

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import clsx from 'clsx';
 import HomePageLayout from './components/Layout/HomePageLayout';
 import ContactSection from './components/Contact/ContactSection';
 
-// export const revalidate = 120;
-// export const dynamic = 'force-dynamic';
+export const revalidate = 120;
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const images = await getCarouselImages();

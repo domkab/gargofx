94585a6f472aff498b61e4fc280e151c70177ea0
domkab <dominykas.bobinas@gmail.com>
2025-08-06 16:10:49 +0300

    huh fix clerk provider ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
   `;
 
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body className={bodyClassName}>
           <BodyFontManager />

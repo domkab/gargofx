5253362c7987fdaa6feb18e1c8dbccceb25ce843
domkab <dominykas.bobinas@gmail.com>
2025-08-06 19:08:11 +0300

    again dash create update

--- src/app/dashboard/update-post/[id]/page.tsx
+++ src/app/dashboard/update-post/[id]/page.tsx
@@ -14,6 +14,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import { DeleteMainImageButton } from '@/app/components/Dashboard/DeleteImage/DeleteMainImageButton';
 import { generateSlug, getSlugSource } from '@/utils/generateSlug';
 import ContentBlockEditor from '@/app/components/PostEditor/ContentBlockEditor';
+export const dynamic = 'force-dynamic';
 
 export default function UpdatePost() {
   const { isSignedIn, user, isLoaded } = useUser();

e83816a35b3d91d001b2857825f64b6c17ebfe82
domkab <dominykas.bobinas@gmail.com>
2025-08-13 22:34:50 +0300

    adding constant for site url

--- src/app/about/metadata.ts
+++ src/app/about/metadata.ts
@@ -1,11 +1,11 @@
 // app/page.tsx
 import type { Metadata } from 'next'
-import { SITE_NAME } from '../../lib/constants'
+import { SITE_NAME, SITE_URL } from '../../lib/constants'
 
 export const revalidate = 300
 
 export const AboutMetadata: Metadata = {
-  metadataBase: new URL('https://www.your-domain.com'),
+  metadataBase: new URL(`${SITE_URL}`),
   title: `${SITE_NAME} — 3D Animation, VFX & Product Renders`,
   description:
     'GargoFX is a 3D animation and VFX studio delivering game trailers, product visualization, and cinematic content for brands and studios.',
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,17 @@
+// site constants
 export const SITE_TITLE = 'Gargofx - 3D Design & Animation Studio';
 export const SITE_NAME = 'gargofx'
 export const DOMAIN_NAME = 'gargofx.com';
+export const SITE_URL = `https://${DOMAIN_NAME}`;
 
+//  social Media Links
 export const facebook = 'https://www.facebook.com/profile.php?id=61574452853021';
 export const instagram = 'https://www.instagram.com/gargofx?igsh=MWNlbmg2endoemVtbA=='
 export const x = 'https://x.com/gargo_ll';
+
 export const phone = '+370 6 155 0539';
+
+// refs
 export const phoneRef = 'tel:+37061550539';
 export const mailRef = 'mailto:laurynas.gargasas@gmail.com'
 export const mail = 'laurynas.gargasas@gmail.com';
